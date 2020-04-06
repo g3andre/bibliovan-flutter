31e2d79a68c477d375f98065e2e0b9993daa3804
g3andre <andre.mmp@gmail.com>
2020-04-06 09:02:48 -0300

    Estrutura básica do projeto

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:bibliovan/turma/index_turma.dart';
+import 'package:bibliovan/utils/route_manager.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  BuildContext _buildContext;
+
+  @override
+  Widget build(BuildContext context) {
+    this._buildContext = context;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BiblioVan"),
+      ),
+      body: Container(
+        child: _body(),
+      ),
+      drawer: _buildDrawer(),
+    );
+  }
+
+  Drawer _buildDrawer() {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+              currentAccountPicture: Image.asset("assets/images/user.png"),
+              accountName: Text("Username"),
+              accountEmail: Text("user@mail.com")),
+          Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.library_books),
+                    title: Text("Empréstimos"),
+                    subtitle: Text("Gerenciar livros emprestados"),
+                    trailing: Icon(Icons.arrow_forward),
+                  ),
+                  ListTile(
+                    onTap: _viewturmas,
+                    leading: Icon(Icons.home),
+                    title: Text("Turmas"),
+                    subtitle: Text("Gerenciar Turmas e Alunos"),
+                    trailing: Icon(Icons.arrow_forward),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings_backup_restore),
+                    title: Text("Devolções"),
+                    subtitle: Text("Devolver livros Emprestados"),
+                    trailing: Icon(Icons.arrow_forward),
+                  )
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+
+  _body() {
+    List<String> items = ["Empréstimos", "Turmas", "Devoluções"];
+    return ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: Icon(Icons.book),
+              title: Text(items[index]),
+            ),
+          );
+        });
+  }
+
+  _viewturmas() {
+    Navigator.pop(this._buildContext);
+    RouteManager.push(this._buildContext, IndexTurma());
+  }
+}
