7ae6ac9cd9ab42ea023a1d7d494fb833c1e68563
g3andre <andre.mmp@gmail.com>
2020-04-15 09:30:01 -0300

    adicionado classes model que compoem o acervo

---
+++ lib/acervo/acervo_api.dart
@@ -0,0 +1,13 @@
+import 'package:http/http.dart' as http;
+
+import 'package:bibliovan/acervo/acervo.dart';
+import 'package:bibliovan/utils/api_utils.dart';
+import 'package:bibliovan/utils/response_managment.dart';
+
+class AcervoApi {
+  static final String API_URL = "${ApiUtils.URL_API}/acervo";
+
+  /* static ResponseManagment<List<Acervo>> getAll() async {
+    
+  } */
+}
