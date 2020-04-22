48881b4a0b6c4449940da2d8669be6c2dccd59ee
g3andre <andre.mmp@gmail.com>
2020-04-21 21:06:04 -0300

    alterado circular progress

--- lib/widgets/circular_progress.dart
+++ lib/widgets/circular_progress.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CircularProgress extends StatelessWidget {
+  Color color;
+  CircularProgress({this.color});
+
   @override
   Widget build(BuildContext context) {
+    if (this.color == null) this.color = Theme.of(context).primaryColor;
     return Container(
       child: Center(
-        child: CircularProgressIndicator(),
+        child: CircularProgressIndicator(
+          backgroundColor: this.color,
+        ),
       ),
     );
   }
